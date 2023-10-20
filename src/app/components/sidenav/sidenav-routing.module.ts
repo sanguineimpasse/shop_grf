83b088988de06983feb83737ec5288cb57a511dd
sanguineimpasse <10geneius@gmail.com>
2023-10-20 14:48:23 +0800

    rebrand, icon fixes, added registration page

---
+++ src/app/components/sidenav/sidenav-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SidenavComponent } from './sidenav.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: SidenavComponent
+  },
+  {
+    path:'login',
+    loadChildren: () => import('../../login/login.module').then(mod=>mod.LoginModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SidenavRoutingModule { }
---
+++ src/app/components/sidenav/sidenav.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidenavComponent } from './sidenav.component';
+import { SidenavRoutingModule } from './sidenav-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    SidenavComponent
+  ],
+  imports: [
+    CommonModule,
+    SidenavRoutingModule
+  ]
+})
+export class SidenavModule { }
